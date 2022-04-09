d11bac34689fa9745cf514c5cf4050a4d87c2cf0
le ngoai ngu <lengoaingu@gmail.com>
2022-04-09 19:29:25 +0700

    initial project react native with typescript

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'comma-dangle': [
+          'error',
+          {
+            arrays: 'never',
+            objects: 'never',
+            imports: 'never',
+            exports: 'never',
+            functions: 'never',
+          },
+        ],
+        // semi: [2, "always"]
+      },
+    },
+  ],
+}
